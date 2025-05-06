ab967607a67c4557b7ccdfcbb76b4dd065b0f13b
Lê Hoàng Khang <iamhoangkhang@icloud.com>
2025-05-06 14:15:11 +0700

    Setup project

---
+++ src/main/java/vn/iuh/ktpm/eduforgepost/EduForgePostApplication.java
@@ -0,0 +1,13 @@
+package vn.iuh.ktpm.eduforgepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduForgePostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduForgePostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/iuh/ktpm/eduforgepost/EduForgePostApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iuh.ktpm.eduforgepost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduForgePostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
